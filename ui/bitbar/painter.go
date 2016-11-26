4ef1c9f584c14bb9980c3c6c9a34be06c8a21f2f
DennisDenuto <dennis.leon39@gmail.com>
2016-11-26 15:03:50 -0800

    Print out the dev name who is working on a red build

--- ui/bitbar/painter.go
+++ ui/bitbar/painter.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/concourse/atc"
 	"time"
+	"github.com/DennisDenuto/igrb/data/diskstore"
+	"github.com/DennisDenuto/igrb/multicast"
+	"strconv"
 )
 
 type Painter struct {
@@ -20,11 +23,26 @@ func JobToString(targetUrl string, build atc.Build) string {
 	commandToInvestigate := fmt.Sprintf("bash=igrb param1=send param2=%s param3=%s param4=%s param5=%d terminal=true", "dev-name", build.PipelineName, build.JobName, build.ID)
 	commandToIgnore := fmt.Sprintf("bash=igrb param1=ignore param2=%s param3=%s param4=%s param5=%d terminal=true", "_", build.PipelineName, build.JobName, build.ID)
 
-	return fmt.Sprintf(`---
-:exclamation: %s/%s %s | href=%s
+	var icon string = ":exclamation:"
+	buildTakenByDev := &multicast.DevLookingIntoBuild{}
+	diskstore.NewDiskPersistor().ReadAndUnmarshal(strconv.Itoa(build.ID), buildTakenByDev)
+	if buildTakenByDev.DevName != "" {
+		icon = ":grey_question:"
+	}
+
+	buildSummaryText := fmt.Sprintf(`---
+%s %s/%s %s | href=%s
 --I got it! | %s
 --Ignore | %s
-Time red: %s`, build.PipelineName, build.JobName, build.Status, targetUrl + build.URL, commandToInvestigate, commandToIgnore, timeElapsed)
+Time red: %s`, icon, build.PipelineName, build.JobName, build.Status, targetUrl + build.URL, commandToInvestigate, commandToIgnore, timeElapsed)
+
+	var buildFooter string
+
+	if buildTakenByDev.DevName != "" {
+		buildFooter = fmt.Sprintf("%s is looking into it!", buildTakenByDev.DevName)
+	}
+
+	return buildSummaryText + "\n" + buildFooter
 }
 
 func (p *Painter) Print() {
